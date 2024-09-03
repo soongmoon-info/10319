935c27fc84f69558667c1b66f52b5f6985c4cbe9
gimoggi123 <eunchanjung0407@gmail.com>
2024-09-03 09:49:11 +0900

    Add files via upload

---
+++ 10319-main/pycoding/learning01.py
@@ -0,0 +1,15 @@
+print('Hello Wold!!')
+
+print(10+20)
+
+num1=int(input('첫번째 숫자 입력'))
+num2=int(input('두번째 숫자 입력'))
+print(num1/num2)
+
+int('20')
+str()
+float()
+#출생 년도를 입력빋이 나이를 출력하는 코드
+#출력 예시
+#생년 입력: 2008
+# 당신의 나이는 17세입니다.
