3f0757319c364916bfe67c43325372da236cc640
gimoggi123 <eunchanjung0407@gmail.com>
2024-09-05 09:46:22 +0900

    Add files via upload

---
+++ 10319-main/10319-main/pycoding/learning01(7).py
@@ -0,0 +1,30 @@
+x=20
+if x>= 10 :
+ print('조건이 ??일 때 출력')
+ print('여기는 언제 출력?')
+
+#나이를 입력받아 나이가 13이하일 경우
+#어린이라고 출력하는 코드를 작성하시오
+age=int(input('나이 입력'))#10
+if age<=13:
+ print('어린이')
+else:
+ if age <=19:
+ print('청소년')
+else:
+  print('딸피')
+
+
+#else:
+#print
+#배터리 잔량을 입력하여 
+#0퍼 이하면 종료를 출력
+#20퍼이하면 저전력 모드를 출력
+# battery <= 10:
+# if bettery <= 10:
+#  print('종료')
+# elif bettery <= 10:
+#  print('충전 ㄱㄱ')
+# elif bettery <= 20:
+#  print('저전력 모드')
+ 
